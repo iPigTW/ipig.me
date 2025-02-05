aae329a78edf72e1a81f9b54799b77edd6f0393c
iPigTW <ipig@ipig.me>
2025-02-05 17:53:11 +0800

    e

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import "@mantine/core/styles.css";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
-import { theme } from "../theme";
 
 export default function App({ Component, pageProps }: any) {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider defaultColorScheme="dark">
       <Head>
-        <title>Mantine Template</title>
+        <title>iPig's Bio</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta content="iPig's Bio" property="og:title" />
+        <meta content="I'm a 13 years old programmer from Taiwan" property="og:description" />
+        <meta content="https://ipig.me/ipig.png" property="og:image" />
+        <meta content="#0047AB" data-react-helmet="true" name="theme-color" />
+        <link rel="shortcut icon" href="/ipig.png" />
       </Head>
       <Component {...pageProps} />
     </MantineProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,66 @@
-import { Button, Group } from "@mantine/core";
+import {
+  Button,
+  Card,
+  Group,
+  Image,
+  Text,
+  Badge,
+  Avatar,
+  Title,
+  MantineProvider,
+} from "@mantine/core";
 
 export default function IndexPage() {
   return (
     <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Card.Section>
+          <Avatar
+            src="/ipig.png"
+            alt="ipig"
+            size={100}
+            display="block"
+            m="auto"
+            mt={20}
+          />
+        </Card.Section>
+        <Text fw={500} mt={10} style={{ textAlign: "center" }}>
+          iPig
+        </Text>
+        <Text size="sm" c="dimmed">
+          I'm a 13 years old programmer from Taiwan
+        </Text>
+        <Button
+          color="#7289da"
+          fullWidth
+          mt="md"
+          radius="md"
+          component="a"
+          href="https://discord.com/users/1113319860760494161"
+        >
+          My Discord
+        </Button>
+        <Button
+          color="black"
+          fullWidth
+          mt="md"
+          radius="md"
+          component="a"
+          href="https://github.com/ipigtw"
+        >
+          My GitHub
+        </Button>
+        <Button
+          color="red"
+          fullWidth
+          mt="md"
+          radius="md"
+          component="a"
+          href="https://youtube.com/@ipigtaiwan"
+        >
+          My YouTube
+        </Button>
+      </Card>
     </Group>
   );
 }
